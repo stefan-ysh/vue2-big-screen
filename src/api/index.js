b64c56755eab3b0fec1ea0a34031a67c3fcaf02e
yshuai <stefan_ysh@foxmail.com>
2023-02-15 17:54:13 +0800

    feat：增加大屏请求 api

---
+++ src/api/index.js
@@ -0,0 +1,18 @@
+// import request from '@/utils/request'
+
+// 新增大屏数据
+export function saveBigscreen(data) {
+  //   return request({
+  //     url: 'code/page/save',
+  //     method: 'post',
+  //     data
+  //   })
+}
+
+// 获取大屏数据
+export function getBigscreenDetail(id) {
+  //   return request({
+  //     url: `code/page/get/one/${id}`,
+  //     method: 'get'
+  //   })
+}
