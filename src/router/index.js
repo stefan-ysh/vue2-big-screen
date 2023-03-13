e8ba9ed1e7304de6cf3f4c407e5060e531eee64c
yshuai <58702199+stefan-ysh@users.noreply.github.com>
2023-03-13 22:45:57 +0800

    test:vercel

--- src/router/index.js
+++ src/router/index.js
@@ -25,9 +25,9 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'hash',
+  mode: 'history',
   // base: process.env.BASE_URL,
-  base: '/vue2-big-screen',
+  // base: '/vue2-big-screen',
   routes
 })
 
